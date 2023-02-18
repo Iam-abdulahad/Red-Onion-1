b04f0d697e290dc2f234db5f8f7e6d199658ce67
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-02-18 09:29:11 +0600

    started a new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './Pages/Shared/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+     <h1>This is another project</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../../images/logo2.png';
+import cartIcon from '../../../images/icons/Path 1.png';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+          return (
+                    <nav class="navbar navbar-expand-lg bg-body-white ">
+                              <div className="container d-flex align-items-center mb-3">
+                                        <div className="me-auto p-2">
+                                                  <img style={{height: 45}} src={logo} alt="" />
+                                              
+                                        </div>
+                                        <div className='p-2'>
+                                                  <img src={cartIcon} alt="" />
+                                                  <Link to='/login' >Login</Link>
+                                                  <Link to='/signup' >SignUp</Link>
+                                        </div>
+                              </div>
+                    </nav>
+          );
+};
+
+export default Header;
