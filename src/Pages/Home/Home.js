dffe0d53f28fe91ae9fcdb94b0f4e76ba9ad77c2
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-02-20 08:14:52 +0600

    finish it later

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
           return (
-                    <div className='bannerSection'>
-                              <div className='hero-text'>
-                                        <h2>Best food waiting for you belly</h2>
-                                        <input type="search" id="site-search" name="q" placeholder='search foods items' />
+                    <div>
+                              <div className='bannerSection'>
+                                        <div className='hero-text'>
+                                                  <h2>Best food waiting for you belly</h2>
+                                                  <input type="search" id="site-search" name="q" placeholder='search foods items' />
 
-                                        <button>Search</button>
+                                                  <button>Search</button>
+                                        </div>
+                              </div>
+
+
+
+                              <div className="container" id='foodMenu'>
+                                        <NavLink className={({ isActive }) => (isActive ? 'active-link' : 'link')} to='/home/breakfast'>Breakfast</NavLink>
+                                        <NavLink className={({ isActive }) => (isActive ? 'active-link' : 'link')} to='/home/lunch'>Lunch</NavLink>
+                                        <NavLink className={({ isActive }) => (isActive ? 'active-link' : 'link')} to='/home/dinner'>Dinner</NavLink>
                               </div>
                     </div>
           );
